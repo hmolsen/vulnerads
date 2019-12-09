ff004a7595c10bbcad3533ff24091fc41fc8a40d
Hannes Molsen <mail@hannesmolsen.de>
2019-12-09 16:33:44 +0100

    changed sosec-local to attacat

--- make_p12_from_pems.sh
+++ make_p12_from_pems.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-openssl pkcs12 -export -in ../sosec-local.de.crt -inkey ../sosec-local.de.key -out ../sosec-local.de.p12 -name sosec -CAfile ../sosec-local.de.chain -chain -passout pass:sosec
+openssl pkcs12 -export -in ../attacat.de.crt -inkey ../attacat.de.key -out ../attacat-8666/conf/attacat.de.p12 -name sosec -CAfile ../attacat.de.chain -chain -passout pass:sosec
 openssl pkcs12 -export -in ../vulnerads.de.crt -inkey ../vulnerads.de.key -out ./src/main/resources/vulnerads.de.p12 -name sosec -CAfile ../vulnerads.de.chain -chain -passout pass:sosec
