cc3402ead266c257e9bead60c6b3b09a4a1b0c5f
Hannes Molsen <mail@hannesmolsen.de>
2019-12-09 21:13:50 +0100

    CORS Übung auf Steroiden

--- src/main/java/de/cqrity/vulnerapp/config/CorsDemoFilter.java
+++ src/main/java/de/cqrity/vulnerapp/config/CorsDemoFilter.java
@@ -11,14 +11,10 @@ public class CorsDemoFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST");
         //response.setHeader("Access-Control-Allow-Origin", "https://attacat.de:8666");
+        //response.setHeader("Access-Control-Allow-Headers", "X-My-Own-Header");
         chain.doFilter(req, res);
-    }
-
-    public void init(FilterConfig filterConfig) {
-    }
-
-    public void destroy() {
     }
 
 }
