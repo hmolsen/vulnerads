e75b29dae4610cc82cc6a88f0904eb0d68a0b296
oz-viadee <oskar.zeino@viadee.de>
2021-03-15 17:06:47 +0100

    Import cleanup

--- src/main/java/de/cqrity/vulnerapp/domain/ClassifiedAdResource.java
+++ src/main/java/de/cqrity/vulnerapp/domain/ClassifiedAdResource.java
@@ -1,6 +1,6 @@
 package de.cqrity.vulnerapp.domain;
 
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.web.multipart.MultipartFile;
 
