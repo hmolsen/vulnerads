96803198c423e8435663dd2b30db5d9c7fba3c21
oz-viadee <oskar.zeino@viadee.de>
2021-03-31 13:35:57 +0200

    Error page implementation is now easier/quicker.

--- solutions/07_error_page/ErrorHandlerController.java
+++ solutions/07_error_page/ErrorHandlerController.java
@@ -2,8 +2,6 @@ package de.cqrity.vulnerapp.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +9,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ErrorHandlerController implements ErrorController{
-
-	Logger logger = LoggerFactory.getLogger(ErrorHandlerController.class);
-
 	
 	@RequestMapping("/error")
 	public ModelAndView handleError(HttpServletRequest request) {
-		/* Logging not necessary since Springboot automatically logs uncaught exceptions
-		Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
-		if (throwable != null) {
-			logger.error("Uncaught exception", throwable);
-		}
-		*/
 		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
 		ModelAndView modelAndView = new ModelAndView("error");
 		modelAndView.addObject("statusCode", statusCode);
