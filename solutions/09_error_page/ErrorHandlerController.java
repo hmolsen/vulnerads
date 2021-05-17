d921c122669563995ab2d830f191e271d9f29c28
oz-viadee <oskar.zeino@viadee.de>
2021-05-17 14:48:59 +0200

    Errorpage task is now a commented walkthrough task

--- solutions/09_error_page/ErrorHandlerController.java
+++ solutions/09_error_page/ErrorHandlerController.java
@@ -7,20 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class ErrorHandlerController implements ErrorController{
-	
+@Controller							// Implementiert den ErrorController von Spring Boot, welcher alle Requests mit ungefangenen Exceptions empfängt.
+public class ErrorHandlerController implements ErrorController {
+
+	// RequestMapping sorgt dafür, dass Requests an die URL "/error" von dieser Methode bearbeitet werden.
+	// "/error" ist dabei die Seite, die von Spring Boot intern bei einer ungefangenen Exception angesteuert wird.
 	@RequestMapping("/error")
 	public ModelAndView handleError(HttpServletRequest request) {
-		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-		ModelAndView modelAndView = new ModelAndView("error");
-		modelAndView.addObject("statusCode", statusCode);
+		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code"); // holt den Statuscode, falls vorhanden, aus der Request, zB. 404 oder 500
+		ModelAndView modelAndView = new ModelAndView("error_page");								// die error_page.jsp Datei wird zur Anzeige der neuen Errorpage genutzt
+		modelAndView.addObject("statusCode", statusCode);										// error_page.jsp empfängt den Statuscode und zeigt ihn auf der Seite an.
 		return modelAndView;
 	}
 
-	// ignore this, required for implementing ErrorController but not used
+	
+	// Bitte einkommentieren, aber ansonsten ignorieren. Wird für die Implementierung von ErrorController benötigt, aber nicht zum Anzeigen einer Errorpage verwendet.
 	@Override
 	public String getErrorPath() {
 		return null;
-	}
+	} 
+	
 }
