31acd2dd8d2b2a4784e0cf548e3aac89e4a76a7b
hmolsen <mail@hannesmolsen.de>
2021-10-11 13:22:30 +0200

    Reflect changes in call of scripit
    Call Parameters are correct

--- src/main/webapp/resources/antivir/antivir.sh
+++ src/main/webapp/resources/antivir/antivir.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo "AntiVir v7.2538 checking file $1 of user $2"
+echo "AntiVir v7.2538 checking file $2 of user $4, user will be blocked if Virus is detected..."
+echo "AntiVir v7.2538 [virus_status] negative. Nothing harmful can come from this file."
 exit 0
