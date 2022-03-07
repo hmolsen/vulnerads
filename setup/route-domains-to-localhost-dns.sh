6f65ee049fcaf1116238ee61a7b04e9f092ac5fc
Oskar <oskar.zeino@viadee.de>
2022-03-07 01:15:53 +0100

    changed vulnerads.de to vulnerads.local

--- setup/route-domains-to-localhost-dns.sh
+++ setup/route-domains-to-localhost-dns.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-echo "127.0.0.1  attacat.de" >> /etc/hosts
-echo "127.0.0.1  vulnerads.de" >> /etc/hosts
+echo "127.0.0.1  vulnerads.local" >> /etc/hosts
+echo "127.0.0.1  cookielogger.local" >> /etc/hosts
+echo "127.0.0.1  mycoolnewblog.local" >> /etc/hosts
+echo "127.0.0.1  cors-test.local" >> /etc/hosts
