446f8eb5f53ea21da2b275ff4fe5a2d60c331d7c
Oskar <oskar.zeino@viadee.de>
2022-03-15 16:38:42 +0100

    fixed aws ip persistence on reboot

--- setup/install_programs.sh
+++ setup/install_programs.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install snap openjdk-14-jre apache2 postgresql python3 python3-pip bleachbit netdiscover nikto -y
+sudo apt install iptables-persistent snap openjdk-14-jre apache2 postgresql python3 python3-pip bleachbit netdiscover nikto -y
 sudo snap install --classic eclipse
 sudo snap install --classic zaproxy
 pip install sqlmap
--- setup/route_aws_ip_to_localhost.sh
+++ setup/route_aws_ip_to_localhost.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if [ $(id -u) -ne 0 ]; then
+echo "Script requires sudo"
+exit 1
+fi
+
+
 # https://askubuntu.com/questions/1168293/redirect-an-external-ip-to-localhost
 
 sudo sysctl -w net.ipv4.conf.enp0s3.route_localnet=1 # to enable redirecting to localhost
 EXTERNAL_IP=169.254.169.254 #change this line to reflect external ipaddress
 sudo iptables -t nat -A OUTPUT -d ${EXTERNAL_IP} -j DNAT --to-destination 127.0.0.1
+
+# persist iptables through reboot
+iptables-save > /etc/iptables/rules.v4
